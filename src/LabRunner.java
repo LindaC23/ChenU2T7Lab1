98257c7647fd7bd42c81c1545599f18aef5c8333
1E10 Laptop <sample@bths.edu>
2022-10-11 12:32:43 -0400

    commit

--- src/LabRunner.java
+++ src/LabRunner.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class LabRunner {
+public class LabRunner {
+    public static void main(String[] args) {
+//        String str = "good morning";
+//        int strLen = str.length();
+//        System.out.print(strLen);
+//
+//        String str1 = "Hello how are you today?";
+//        System.out.println(str1.indexOf("you"));
+//        System.out.println(str1.indexOf("You"));
+
+//        String myString = "AP CSA is awesome!";
+//        String s = myString.substring(4,5);
+//        System.out.println(s);
+//        String awe = myString.substring(10,18);
+//        System.out.println(awe);
+//        String aweStr = myString.substring(10,13);
+//       System.out.println(aweStr);
+
+//        String origStr ="Hello how are you today?";
+//        String str9 = origStr.substring(6,9);
+//        System.out.println(str9);
+
+//        String blah = "What's for dinner?";
+//        String ugh = blah.substring(11,17);
+//        System.out.println(ugh);
+
+        String blah2 = "What's for dinner?";
+        String ugh2 = blah2.substring(11);
+        System.out.println(ugh2);
+        String ugh3 = blah2.substring(11,18);
+        System.out.println(ugh3);
+
+    }
 }
